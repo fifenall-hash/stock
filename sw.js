1dd57d5904c67ff45ded0f91a329f0485717f4c3
fifenall-hash <fifenall@gmail.com>
2026-06-24 13:25:43 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'stock-app-cache-v1';
+const CACHE_NAME = 'stock-app-cache-v3';
 const urlsToCache = [
   './index.html',
   './manifest.json',
-  './icon.png'
+  './icon.png',
+  './icon.ico'
 ];
 
-// 설치 시 캐시 저장
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -15,7 +15,6 @@ self.addEventListener('install', event => {
   );
 });
 
-// 네트워크 요청 처리 (오프라인 시 캐시 사용)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
