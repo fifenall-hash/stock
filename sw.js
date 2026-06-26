fedd42406d33430d6b8ed93c58c93f5e0951f03e
fifenall-hash <fifenall@gmail.com>
2026-06-27 07:46:05 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,24 @@
-const CACHE_NAME = 'stock-app-cache-v3';
-const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './icon.png',
-  './icon.ico'
-];
+// 오프라인 캐시 기능을 제거하고, PWA 앱 설치 조건(fetch 이벤트 존재)만 충족시키는 코드
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+    // 설치 즉시 활성화
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    // 기존에 남아있던 불필요한 캐시가 있다면 모두 삭제
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+    // 캐시를 확인하지 않고 무조건 실시간 네트워크 요청만 수행
+    event.respondWith(fetch(event.request));
 });
